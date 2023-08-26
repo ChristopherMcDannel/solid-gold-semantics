b96e5ae3e79e1ad1e12b26957268be0344a2ef60
Christopher McDannel <137365457+ChristopherMcDannel@users.noreply.github.com>
2023-08-26 11:55:23 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# main.py
+
+import threading
+import queue
+import whisper
+from recorder import record_audio
+from transcriber import transcribe_audio
+
+if __name__=="__main__":
+    model = whisper.load_model("small.en")
+
+    # Shared queue to communicate between threads
+    audio_queue = queue.Queue()
+
+    # Start threads
+    mic_thread = threading.Thread(target=record_audio, args=(audio_queue,))
+    write_thread = threading.Thread(target=transcribe_audio, args=(audio_queue, model))
+    
+    mic_thread.start()
+    write_thread.start()
+    
+    mic_thread.join()
+    write_thread.join()
